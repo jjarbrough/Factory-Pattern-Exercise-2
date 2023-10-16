600ee6493e12b4f38349314de184833dda9db981
James Yarbrough <jjarbrough@gmail.com>
2023-10-16 10:55:09 -0500

    finished the exercise

---
+++ FactoryPatternExercise2/SQLDataAccess.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryPatternExercise2
+{
+    internal class SQLDataAccess : IDataAccess
+    {
+        public List<Product> LoadData()
+        {
+            Console.WriteLine("I am loading from a SQL database");
+            List<Product> products = new List<Product>();
+            products.Add(new Product());
+            products.Add(new Product());
+            return products;
+        }
+
+        public void SaveData()
+        {
+            Console.WriteLine("I am saving data to a SQL database");
+        }
+    }
+}
